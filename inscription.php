230c437067f192421817f13774f558b2e22289ac
Houthoofd <houthoofd.benoit48@gmail.com>
2021-05-12 21:39:11 +0200

    mise en place de la page d'inscription et de connexion

---
+++ inscription.php
@@ -0,0 +1,36 @@
+<div id="inscription">
+  <form action="post">
+    <div class="input_inscription">
+      <label for="name">Nom</label>
+      <input type="text" class="input"></br>
+    </div>
+    <div class="input_inscription">
+      <label for="prenom">Prénom</label>
+      <input type="text" class="input"></br>
+    </div>
+    <div class="input_inscription">
+      <label for="phone">Pseudonyme</label>
+      <input type="text" class="input"></br>
+    </div>
+    <div class="input_inscription">
+      <label for="mail">E-mail</label>
+      <input type="text" class="input"></br>
+    </div>
+    <div class="input_inscription">
+      <label for="phone">N° Téléphone</label>
+      <input type="text" class="input"></br>
+    </div>
+    <div class="input_inscription">
+      <label for="date_naissance">Date de naissance</label>
+      <input type="text" class="input"></br>
+    </div>
+    <div class="input_inscription">
+      <label for="password">Mot de passe</label>
+      <input type="text" class="input"></br>
+    </div>
+    <div class="input_inscription">
+      <label for="second_validation">Confirmer mot de passe</label>
+      <input type="text" class="input"></br>
+    </div>
+  </form>
+<div>
---
+++ signin.php
@@ -0,0 +1,12 @@
+<div id="login">
+  <form action="post">
+    <div class="input_login">
+      <label for="phone">Pseudonyme</label>
+      <input type="text" class="input"></br>
+    </div>
+    <div class="input_login">
+      <label for="phone">Mot de passe</label>
+      <input type="text" class="input"></br>
+    </div>
+  </form>
+</div>
